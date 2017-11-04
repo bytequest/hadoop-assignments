66393d446a3c3b2d514045735c5528446276fcd2
bytequest <bytequest@bytequest.net>
2017-11-04 00:48:45 -0700

    Initial commit

---
+++ wordcount/src/main/java/net/bytequest/hadoop/WordCountDriver.java
@@ -0,0 +1,55 @@
+package net.bytequest.hadoop;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordCountDriver extends Configured implements Tool
+{
+	
+	public static void main(String[] args) throws Exception
+	{
+		int exitCode = ToolRunner.run(new WordCountDriver(), args);
+		System.exit(exitCode);
+	}		
+ 
+	public int run(String[] args) throws Exception 
+	{
+		if (args.length != 2) 
+		{
+			System.err.printf("Usage: %s needs two arguments, input and output files\n", getClass().getSimpleName());
+			return -1;
+		}
+		
+		Job job = new Job();
+		job.setJarByClass(WordCountDriver.class);
+		job.setJobName("WordCounter");
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		int returnValue = job.waitForCompletion(true) ? 0:1;
+		
+		if(job.isSuccessful()) 
+		{
+			System.out.println("Job was successful");
+		} 
+		else if(!job.isSuccessful()) 
+		{
+			System.out.println("Job was not successful");			
+		}
+		return returnValue;
+	}
+}
---
+++ wordcount/src/main/java/net/bytequest/hadoop/WordCountReducer.java
@@ -0,0 +1,24 @@
+package net.bytequest.hadoop;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends Reducer<Text,IntWritable,Text,IntWritable> 
+{
+	private IntWritable result = new IntWritable();
+
+	protected void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException 
+	{
+		int sum = 0;
+	    for(IntWritable value : values)
+	    {
+			sum += value.get();
+		}
+	
+	    result.set(sum);
+	    context.write(key, result);
+	}	
+}
